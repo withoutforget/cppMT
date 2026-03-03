4969e4ec8410ee73a2dc0a1cdc8e210fa9a14607
Roman Kovach <idcfyoy@gmail.com>
2026-03-03 10:53:40 +0300

    misc: linted

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include <thread>
 #include <vector>
 
-
 class RingBuffer {
 private:
     std::vector<int> m_data;
@@ -13,7 +12,8 @@ private:
     std::atomic<size_t> m_end;
 
 public:
-    RingBuffer(size_t sz) : m_data(sz, 0), m_seq(sz), m_begin(0ull), m_end(0ull) {
+    RingBuffer(size_t sz)
+        : m_data(sz, 0), m_seq(sz), m_begin(0ull), m_end(0ull) {
         for (size_t i = 0; i < sz; i++)
             m_seq[i] = i;
     }
@@ -31,7 +31,8 @@ public:
                 std::memory_order::relaxed
             ))
             return false;
-        while (m_seq[idx % m_data.size()].load(std::memory_order::acquire) < idx)
+        while (m_seq[idx % m_data.size()].load(std::memory_order::acquire) < idx
+        )
             continue;
         m_data[idx % m_data.size()] = v;
         m_seq[idx % m_data.size()].store(idx + 1, std::memory_order::release);
@@ -49,11 +50,14 @@ public:
                 std::memory_order::relaxed
             ))
             return false;
-        while (m_seq[idx % m_data.size()].load(std::memory_order::acquire) < idx + 1)
+        while (m_seq[idx % m_data.size()].load(std::memory_order::acquire) <
+               idx + 1)
             continue;
         r = m_data[idx % m_data.size()];
 
-        m_seq[idx % m_data.size()].store(idx + m_data.size(), std::memory_order::release);
+        m_seq[idx % m_data.size()].store(
+            idx + m_data.size(), std::memory_order::release
+        );
         return true;
     }
 };
